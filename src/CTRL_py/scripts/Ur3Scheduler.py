be179abd65ae574dad15bad4a5027e762c9c83eb
QuokeCola <QuokeCola@iCloud.com>
2021-11-13 18:37:35 -0600

    Fixed main_exec.py

---
+++ src/CTRL_py/scripts/Ur3Scheduler.py
@@ -0,0 +1,63 @@
+from Ur3Interface import *
+
+class UR3Scheduler():
+    def convert_trace_to_angle(self, trace, vel, accel):
+        converted_thetas = []
+        print ("Perform Inverse Kinematics Calculation")
+        print ("\n")
+        for i in trace:
+            converted_thetas.append(self.__inv_kinetics(*i))
+        return converted_thetas
+
+    def __inv_kinetics(self, xWgrip, yWgrip, zWgrip, yaw_WgripDegree):
+        """Calculate the joint angles for given position.
+
+        :param xWgrip: target X in world frame
+        :param yWgrip: target Y in world frame
+        :param zWgrip: target Z in world frame
+        :param yaw_WgripDegree: YAW angle of the plate
+        :return:
+        """
+        L1 = 0.152
+        L2 = 0.12
+        L3 = 0.244
+        L4 = 0.093
+        L5 = 0.213
+        L6 = 0.083
+        L7 = 0.083
+        L8 = 0.082
+        L9 = 0.0535
+        L10 = 0.059
+        xgrip = xWgrip + 0.15
+        ygrip = yWgrip - 0.15
+        zgrip = zWgrip - 0.01
+        xcen = xgrip - L9 * np.cos(np.deg2rad(yaw_WgripDegree))
+        ycen = ygrip - L9 * np.sin(np.deg2rad(yaw_WgripDegree))
+        zcen = zgrip
+        alpha = np.arctan2(0.027 + L6, np.sqrt(xcen**2+ycen**2-(L6 + 0.027)**2))
+        theta1 = np.arctan2(ycen,xcen) -  alpha
+        theta6 = np.pi/2 + theta1-np.deg2rad(yaw_WgripDegree)
+        x3end = xcen - L7*np.cos(theta1) + (0.027+L6)*np.sin(theta1)
+        y3end = ycen - L7*np.sin(theta1) - (0.027+L6)*np.cos(theta1)
+        z3end = zcen + L10 + L8
+        M1 = np.sqrt(x3end**2 + (z3end-L1)**2 + y3end**2)
+        omega = np.arctan2((z3end - L1), np.sqrt(x3end**2+y3end**2))
+        theta3 = np.pi - np.arccos((L3**2 + L5**2 - M1**2)/(2*L3*L5))
+        theta2 = -(omega + np.arccos((L3**2 + M1**2 - L5**2)/(2*L3*M1)))
+        theta4 = -(theta3 + theta2)
+        theta5 = -np.pi/2
+        thetas = self.__convert_angles(theta1, theta2, theta3, theta4, theta5, theta6)
+        return thetas
+
+    def __convert_angles(self, theta1, theta2, theta3, theta4, theta5, theta6):
+        """Convert angles to joint angles for robot
+        """
+        return_value = [None, None, None, None, None, None]
+
+        return_value[0] = theta1 + math.pi
+        return_value[1] = theta2
+        return_value[2] = theta3
+        return_value[3] = theta4 - (0.5*math.pi)
+        return_value[4] = theta5
+        return_value[5] = theta6
+        return return_value
--- src/CTRL_py/scripts/main_exec.py
+++ src/CTRL_py/scripts/main_exec.py
@@ -11,18 +11,23 @@ import yaml
 import sys
 from math import pi
 from Ur3Interface import *
+from Ur3Scheduler import *
+from LaserInterface import *
 
+ur3IF = UR3Interface()
+ur3SKD = UR3Scheduler()
+laserIF = LaserInterface()
 # Position for UR3 not blocking the camera
-go_away = [270*PI/180.0, -90*PI/180.0, 90*PI/180.0, -90*PI/180.0, -90*PI/180.0, 135*PI/180.0]
+go_away = [270*math.pi/180.0, -90*math.pi/180.0, 90*math.pi/180.0, -90*math.pi/180.0, -90*math.pi/180.0, 135*math.pi/180.0]
 
 # Store world coordinates of sanitizer and the target place
-xw_yw_S = []
-xw_yw_M = []
+xw_yw_S = [0.3,0.15,0.15,0]
+xw_yw_M = [0.3,0.15,0.028,0]
 # 20Hz
 SPIN_RATE = 20
 
 # UR3 home location
-home = [0*PI/180.0, 0*PI/180.0, 0*PI/180.0, 0*PI/180.0, 0*PI/180.0, 0*PI/180.0]
+home = [0*math.pi/180.0, 0*math.pi/180.0, 0*math.pi/180.0, 0*math.pi/180.0, 0*math.pi/180.0, 0*math.pi/180.0]
 
 # UR3 current position, using home position for initialization
 current_position = copy.deepcopy(home)
@@ -40,180 +45,32 @@ current_position_set = False
 
 image_shape_define = False
 
-
-"""
-Whenever ur3/gripper_input publishes info this callback function is called.
-"""
-def input_callback(msg):
-
-    global digital_in_0
-    digital_in_0 = msg.DIGIN
-    digital_in_0 = digital_in_0 & 1 # Only look at least significant bit, meaning index 0
-    global suction_state
-
-    suction_state = msg.DIGIN
-
-
-"""
-Whenever ur3/position publishes info, this callback function is called.
-"""
-def position_callback(msg):
-
-    global thetas
-    global current_position
-    global current_position_set
-
-    thetas[0] = msg.position[0]
-    thetas[1] = msg.position[1]
-    thetas[2] = msg.position[2]
-    thetas[3] = msg.position[3]
-    thetas[4] = msg.position[4]
-    thetas[5] = msg.position[5]
-
-    current_position[0] = thetas[0]
-    current_position[1] = thetas[1]
-    current_position[2] = thetas[2]
-    current_position[3] = thetas[3]
-    current_position[4] = thetas[4]
-    current_position[5] = thetas[5]
-
-    current_position_set = True
-
-
-"""
-Function to control the suction cup on/off
-"""
-def gripper(pub_cmd, loop_rate, io_0):
-
-    global SPIN_RATE
-    global thetas
-    global current_io_0
-    global current_position
-
-    error = 0
-    spin_count = 0
-    at_goal = 0
-
-    current_io_0 = io_0
-
-    driver_msg = command()
-    driver_msg.destination = current_position
-    driver_msg.v = 1.0
-    driver_msg.a = 1.0
-    driver_msg.io_0 = io_0
-    pub_cmd.publish(driver_msg)
-
-    while(at_goal == 0):
-
-        if( abs(thetas[0]-driver_msg.destination[0]) < 0.0005 and \
-            abs(thetas[1]-driver_msg.destination[1]) < 0.0005 and \
-            abs(thetas[2]-driver_msg.destination[2]) < 0.0005 and \
-            abs(thetas[3]-driver_msg.destination[3]) < 0.0005 and \
-            abs(thetas[4]-driver_msg.destination[4]) < 0.0005 and \
-            abs(thetas[5]-driver_msg.destination[5]) < 0.0005 ):
-
-            #rospy.loginfo("Goal is reached!")
-            at_goal = 1
-
-        loop_rate.sleep()
-
-        if(spin_count >  SPIN_RATE*5):
-
-            pub_cmd.publish(driver_msg)
-            rospy.loginfo("Just published again driver_msg")
-            spin_count = 0
-
-        spin_count = spin_count + 1
-
-    return error
-
-
 """
 Move robot arm from one position to another
 """
-def move_arm(pub_cmd, loop_rate, dest, vel, accel):
 
-    global thetas
-    global SPIN_RATE
+def move_block(start_pose, discrete_route_set, end_pose, vel, accel):
 
-    error = 0
-    spin_count = 0
-    at_goal = 0
-
-    driver_msg = command()
-    driver_msg.destination = dest
-    driver_msg.v = vel
-    driver_msg.a = accel
-    driver_msg.io_0 = current_io_0
-    pub_cmd.publish(driver_msg)
-
-    loop_rate.sleep()
-
-    while(at_goal == 0):
-
-        if( abs(thetas[0]-driver_msg.destination[0]) < 0.0005 and \
-            abs(thetas[1]-driver_msg.destination[1]) < 0.0005 and \
-            abs(thetas[2]-driver_msg.destination[2]) < 0.0005 and \
-            abs(thetas[3]-driver_msg.destination[3]) < 0.0005 and \
-            abs(thetas[4]-driver_msg.destination[4]) < 0.0005 and \
-            abs(thetas[5]-driver_msg.destination[5]) < 0.0005 ):
-
-            at_goal = 1
-            #rospy.loginfo("Goal is reached!")
-
-        loop_rate.sleep()
-
-        if(spin_count >  SPIN_RATE*5):
-
-            pub_cmd.publish(driver_msg)
-            rospy.loginfo("Just published again driver_msg_gg")
-            spin_count = 0
-
-        spin_count = spin_count + 1
-
-    return error
-
-
-def move_block(pub_cmd, loop_rate, start_xw_yw_zw, target_xw_yw_zw,end_xw_yw_zw, vel, accel):
-
-    """
-    start_xw_yw_zw: where to pick up a block in global coordinates
-    target_xw_yw_zw: where to put the block at a given place in global coordinates
-    end_xw_yw_zw: where to place a block in global coordinates
-
-    hint: you will use lab_invk(), gripper(), move_arm() functions to
-    pick and place a block
-
-    """
-    # ========================= Student's code starts here =========================
-
-    # global variable1
-    # global variable2
-    angle_start = lab_invk(start_xw_yw_zw[0], start_xw_yw_zw[1], start_xw_yw_zw[2], 0)
-    angle_target = lab_invk(target_xw_yw_zw[0], target_xw_yw_zw[1],target_xw_yw_zw[2] , 0)
-    angle_end = lab_invk(end_xw_yw_zw[0], end_xw_yw_zw[1], end_xw_yw_zw[2], 0)
-    angle_start_upper = lab_invk(start_xw_yw_zw[0], start_xw_yw_zw[1], start_xw_yw_zw[2]+0.1, 0)
-    angle_end_upper = lab_invk(end_xw_yw_zw[0], end_xw_yw_zw[1], end_xw_yw_zw[2]+0.1, 0)
-
-    vel = 4.0
-    accel = 4.0
-    #Define locations from left to right as 1, 2, 3. Define height from low to high as 1 2 3
-    move_arm(pub_cmd, loop_rate, angle_start_upper, vel, accel)
-    move_arm(pub_cmd, loop_rate, angle_start, vel, accel)
-    gripper(pub_cmd, loop_rate, suction_on)
+    angles = ur3SKD.convert_trace_to_angle([[start_pose[0], start_pose[1], start_pose[2] + 0.05, start_pose[3]], start_pose], vel, accel)
+    ur3IF.set_gripper(suction_on)
+    for i in angles:
+        ur3IF.set_angle(i, vel, accel)
     time.sleep(1.0)
-    if (suction_state == False):
-        gripper(pub_cmd, loop_rate, suction_off)
-        move_arm(pub_cmd, loop_rate, go_away, vel, accel)
+    if (ur3IF.get_gripper_sensor() == False):
+        ur3IF.set_gripper(suction_off)
+        ur3IF.set_angle(go_away, vel, accel)
         rospy.loginfo("Block Missing, Mission Skipped!")
+        return 1
     else:
-        move_arm(pub_cmd, loop_rate, angle_start_upper, vel, accel)
-        move_arm(pub_cmd, loop_rate, angle_target, vel, accel)
+        angles = ur3SKD.convert_trace_to_angle(discrete_route_set, vel, accel)
+        for i in angles:
+            ur3IF.set_angle(i, vel, accel)
         time.sleep(2.0)
-        move_arm(pub_cmd, loop_rate, angle_end_upper, vel, accel)
-        move_arm(pub_cmd, loop_rate, angle_end, vel, accel)
-        gripper(pub_cmd, loop_rate, suction_off)
-        move_arm(pub_cmd, loop_rate, angle_end_upper, vel, accel)
+    angles = ur3SKD.convert_trace_to_angle([[end_pose[0], end_pose[1], end_pose[2] + 0.05, end_pose[3]], end_pose], vel, accel)
+    for i in angles:
+        ur3IF.set_angle(i, vel, accel)
+    ur3IF.set_gripper(suction_off)
+    #Define locations from left to right as 1, 2, 3. Define height from low to high as 1 2 3
     time.sleep(1.0)
 
     error = 0
@@ -235,15 +92,7 @@ def main():
     # global variable2
 
     # Initialize ROS node
-    #rospy.init_node('lab5node')
-
-    # Initialize publisher for ur3/command with buffer size of 10
-    pub_command = rospy.Publisher('ur3/command', command, queue_size=10)
-
-    # Initialize subscriber to ur3/position & ur3/gripper_input and callback fuction
-    # each time data is published
-    sub_position = rospy.Subscriber('ur3/position', position, position_callback)
-    sub_input = rospy.Subscriber('ur3/gripper_input', gripper_input, input_callback)
+    rospy.init_node('CTRL_py')
 
     # Check if ROS is ready for operation
     while(rospy.is_shutdown()):
@@ -254,32 +103,21 @@ def main():
 
     vel = 4.0
     accel = 4.0
-    move_arm(pub_command, loop_rate, go_away, vel, accel)
-
-    ic = ImageConverter(SPIN_RATE)
+    print ("Tend to go to initial angle")
+    ur3IF.set_angle(go_away, vel, accel)
+    print ("Initial angle reached")
     time.sleep(5)
 
     
     """
     Initial and final postion should be xw_yw_S, medimum place should be xw_yw_M which should be detected by the camera.
     """
-    move_block(pub_command, loop_rate, xw_yw_S, xw_yw_M,xw_yw_S, 4.0, 4.0)
-    move_arm(pub_command, loop_rate, go_away, vel, accel)
+
+    move_block(xw_yw_S, [xw_yw_M,xw_yw_S,xw_yw_M,xw_yw_S,xw_yw_M,xw_yw_S,xw_yw_M,xw_yw_S], xw_yw_S, 4.0, 4.0)
+    ur3IF.set_angle(go_away, vel, accel)
     rospy.loginfo("Task Completed!")
     print("Use Ctrl+C to exit program")
     rospy.spin()
-
-
-#Q13 = [120 * pi / 180.0, -56 * pi / 180.0, 124 * pi / 180.0, -158 * pi / 180.0, -90 * pi / 180.0, 0 * pi / 180.0]
-
-
-#def main():
-
-    #rospy.init_node('CTRL_node')
-    #ur3IF = UR3Interface()
-    #rospy.loginfo('hello, world')
-    #loop_rate = rospy.Rate(20)
-    #ur3IF.set_angle(Q13, 4.0, 4.0)
 
 
 if __name__ == '__main__':
